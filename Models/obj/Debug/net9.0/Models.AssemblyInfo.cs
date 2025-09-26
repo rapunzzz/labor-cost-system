8df32e07cfc1643c799c63668029da46b79ec6a4
rapunzzz <kurniathaariq55@gmail.com>
2025-09-26 11:22:01 +0700

    Upd: Remove Unassigned Models in Comparison Summary

--- Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cdf124feedf51e5edf2961bae4afdff652c298d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a02cb02853526f89d28199b7d00c2994819eb27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
